b0d49114aa794de4fc4eb671e22abf0da5025f9a
Albert Hovakimyan <HovakimyanAB@gmail.com>
2021-07-29 13:41:59 +0400

    only birds and planes

--- main.py
+++ main.py
@@ -22,14 +22,15 @@ net.setInputSwapRB(True)
 
 while True:
     success, img = cap.read()
-    classIds, confs, bbox = net.detect(img, confThreshold=0.5)
+    classIds, confs, bbox = net.detect(img, confThreshold=0.25)
     print(classIds, bbox)
 
     if len(classIds) != 0:
         for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
-            cv2.rectangle(img, box, color=(0, 255, 0), thickness=2)
-            cv2.putText(img, classNames[classId-1].upper(), (box[0]+10, box[1]+30),
-                        cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+            if classId == 5 or classId == 16:
+                cv2.rectangle(img, box, color=(0, 255, 0), thickness=2)
+                cv2.putText(img, classNames[classId-1].upper(), (box[0]+10, box[1]+30),
+                            cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
 
     cv2.imshow("Output", img)
     cv2.waitKey(1)
